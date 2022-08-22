ca0b9e5b625d2f4eeb2b5146167df0750b538356
devhj1229 <devhj@urpsys.com>
2022-08-22 17:21:34 +0900

    출력로그추가

--- app.js
+++ app.js
@@ -3,3 +3,4 @@
 */
 console.log('app.js');
 console.log('sourceTree로 커밋하기');
+console.log('amend 옵션으로 commit 수정하기');
--- board.js
+++ board.js
@@ -4,3 +4,4 @@
 console.log('board');
 console.log('develop 브랜치에서 추가함');
 console.log('feat/board 에서 추가함');
+console.log('amend 옵션으로 commit 수정하기');
